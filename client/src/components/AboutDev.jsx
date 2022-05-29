2b120ed1419960362e9d54b2c9292efa4f88aef0
Joshua <joshuatauro45@gmail.com>
2022-05-29 22:38:55 +0530

    Fixed dark mode bugs

--- client/src/components/AboutDev.jsx
+++ client/src/components/AboutDev.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const AboutDev = () => {
   return (
-    <div className="w-full bg-cta-fade rounded-md">
+    <div className="w-full bg-cta rounded-md dark:bg-dark-cta-fade dark:">
       <div className="px-2">
-        <p className="text-cta-fade-text text-[14px] rounded-md py-2">If you liked this project, feel free to star this project on <a href="https://github.com/joshuatauro45" className="text-cta">Github</a>. If you want to know more about me and some of my other projects, feel free to head over to my <a href="https://joshuatauro.vercel.app" className="text-cta">portfolio</a></p>
+        <p className="text-white text-[14px] rounded-md py-2">If you liked this project, feel free to star this project on <a href="https://github.com/joshuatauro45" className="text-cta">Github</a>. If you want to know more about me and some of my other projects, feel free to head over to my <a href="https://joshuatauro.vercel.app" className="text-cta">portfolio</a></p>
       </div>
     </div>
   )
--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -7,7 +7,7 @@ const SideNav = ({tab}) => {
       <div className="">
         <ul className="sticky top-[100px]">
           <Link to="/feed">
-            <div to="/feed" className={` py-1.5 mb-2 ${tab === 1 ? 'bg-gray-200 border-r-4 border-cta' : ''}`}>
+            <div to="/feed" className={` py-1.5 mb-2 ${tab === 1 ? 'bg-gray-200 dark:bg-dark-cta-fade  border-r-4 border-cta' : ''} dark:text-white`}>
               <div className="flex items-center w-9/12 m-auto">
                 <GlobeIcon className="h-6 mr-1 w-6" />
                 <h1 className="text-sm">Questions</h1>
@@ -15,7 +15,7 @@ const SideNav = ({tab}) => {
             </div>
           </Link>
           <Link to="/tags">
-            <div className={` py-1.5 mb-2 ${tab === 2 ? 'bg-gray-200 border-r-4 border-cta' : ''}`}>
+            <div className={` py-1.5 mb-2 ${tab === 2 ? 'bg-gray-200 border-r-4 border-cta' : ''} dark:text-white`}>
               <div className="flex items-center w-9/12 m-auto">
                 <TagIcon className="h-6 mr-1 w-6" />
                 <h1 className="text-sm">Tags</h1>
@@ -23,7 +23,7 @@ const SideNav = ({tab}) => {
             </div>
           </Link>
           <Link to="/users">
-            <div className={` py-1.5 mb-2 ${tab === 3 ? 'bg-gray-200 border-r-4 border-cta' : ''}`}>
+            <div className={` py-1.5 mb-2 ${tab === 3 ? 'bg-gray-200 dark:bg-dark-cta-fade  border-r-4 border-cta dark:text-white' : 'dark:text-white'}`}>
               <div className="flex items-center w-9/12 m-auto">
                 <UserIcon className="h-6 mr-1 w-6" />
                 <h1 className="text-sm">Users</h1>
